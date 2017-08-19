4a99345ae121dcefb2f635f2e7bd4888906a1f3d
Nattapon Chittnarong <smarty_0123@hotmail.com>
2017-08-19 15:18:26 +0700

    Finish All ConstraintLayout

--- lab02/MyApplication/app/src/main/java/myfirstapp/example/com/myapplication/MainActivity.java
+++ lab02/MyApplication/app/src/main/java/myfirstapp/example/com/myapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relativelayout_lab02_1);
+        setContentView(R.layout.activity_constraintlayout_lab02_3);
     }
 }
