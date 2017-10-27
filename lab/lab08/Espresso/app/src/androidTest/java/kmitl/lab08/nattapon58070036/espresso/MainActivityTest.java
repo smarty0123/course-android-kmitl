b1db25e7b7a55a230388760e61c0751c0f068926
Nattapon Chittnarong <smarty_0123@hotmail.com>
2017-10-27 18:06:37 +0700

    Finish Homework08 Espresso

--- lab/lab08/Espresso/app/src/androidTest/java/kmitl/lab08/nattapon58070036/espresso/MainActivityTest.java
+++ lab/lab08/Espresso/app/src/androidTest/java/kmitl/lab08/nattapon58070036/espresso/MainActivityTest.java
@@ -15,7 +15,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
